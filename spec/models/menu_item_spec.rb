3a858ab0c6afd2426e3adec5afa6edad5ddcb569
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2025-09-09 21:29:35 -0300

    Level 1: Create models spec

---
+++ spec/models/menu_item_spec.rb
@@ -0,0 +1,89 @@
+require "rails_helper"
+
+RSpec.describe MenuItem, type: :model do
+  let(:name) { "Sample Item" }
+  let(:price_in_cents) { 1500 }
+  let(:restriction_type) { :no_restriction }
+
+  let(:menu) { create(:menu) }
+  let(:menu_item) { build(:menu_item, menu: menu, name: name, price_in_cents: price_in_cents, restriction_type: restriction_type) }
+
+  describe "validations" do
+    it "is invalid without a name" do
+      menu_item.name = nil
+
+      expect(menu_item).not_to be_valid
+    end
+
+    it "is invalid without a price_in_cents" do
+      menu_item.price_in_cents = nil
+
+      expect(menu_item).not_to be_valid
+    end
+
+    it "is invalid with a negative price_in_cents" do
+      menu_item.price_in_cents = -100
+
+      expect(menu_item).not_to be_valid
+    end
+
+    it "is invalid with a float price_in_cents" do
+      menu_item.price_in_cents = 100.50
+
+      expect(menu_item).not_to be_valid
+    end
+
+    it "raises an error with an invalid restriction_type" do
+      expect {
+        menu_item.restriction_type = :invalid
+      }.to raise_error(ArgumentError)
+    end
+
+    it "accepts valid restriction_type values" do
+      menu_item.restriction_type = :vegan
+
+      expect(menu_item).to be_valid
+    end
+  end
+
+  describe "scopes" do
+    let!(:item_none) { create(:menu_item, menu: menu, restriction_type: :no_restriction) }
+    let!(:item_vegan) { create(:menu_item, menu: menu, restriction_type: :vegan) }
+    let!(:item_lactose) { create(:menu_item, menu: menu, restriction_type: :lactose_free) }
+    let!(:item_gluten) { create(:menu_item, menu: menu, restriction_type: :gluten_free) }
+
+    it "returns only vegan items" do
+      expect(MenuItem.vegan).to contain_exactly(item_vegan)
+    end
+
+    it "returns only lactose_free items" do
+      expect(MenuItem.lactose_free).to contain_exactly(item_lactose)
+    end
+
+    it "returns only gluten_free items" do
+      expect(MenuItem.gluten_free).to contain_exactly(item_gluten)
+    end
+  end
+
+  describe "associations" do
+    it { should belong_to(:menu) }
+  end
+
+  describe "enums" do
+    it { should define_enum_for(:restriction_type).with_values(no_restriction: 0, vegan: 1, lactose_free: 2, gluten_free: 3) }
+  end
+
+  describe "factories" do
+    it "has a valid factory" do
+      expect(build(:menu_item)).to be_valid
+    end
+  end
+
+  describe "functions" do
+    it "price returns the correct dollar amount" do
+      menu_item.price_in_cents = 1234
+
+      expect(menu_item.price).to eq(12.34)
+    end
+  end
+end
---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe Menu, type: :model do
+  describe "validations" do
+    it "is invalid without a name" do
+      menu = build(:menu, name: nil)
+
+      expect(menu).not_to be_valid
+    end
+
+    it "is invalid without a menu_type" do
+      menu = build(:menu, menu_type: nil)
+
+      expect(menu).not_to be_valid
+    end
+  end
+
+  describe "factories" do
+    it "has a valid factory" do
+      expect(build(:menu)).to be_valid
+    end
+  end
+
+  describe "callbacks" do
+    it "parameterizes menu_type before validation" do
+      menu = build(:menu, menu_type: "Main Course")
+      menu.valid?
+      expect(menu.menu_type).to eq("main-course")
+    end
+  end
+
+  describe "associations" do
+    it "destroys associated menu_items when deleted" do
+      menu = create(:menu)
+      create(:menu_item, menu: menu)
+
+      expect { menu.destroy }.to change(MenuItem, :count).by(-1)
+    end
+  end
+end
