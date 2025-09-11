e0f3ea0f0644200f818f4cb89978f5e6ba745925
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2025-09-10 21:34:22 -0300

    Update MenuItems spec with edge-cases and new route

--- spec/requests/menu_items_request_spec.rb
+++ spec/requests/menu_items_request_spec.rb
@@ -1,18 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "MenuItemsRequest", type: :request do
+  let!(:restaurant) { create(:restaurant, name: "Sample Restaurant") }
   let!(:starter_menu) { create(:menu, name: "Starters", menu_type: "starter") }
   let!(:main_menu) { create(:menu, name: "Main Course", menu_type: "main-course") }
 
   before do
-    create(:menu_item, name: "Bruschetta", price_in_cents: 700, restriction_type: :vegan, menu: starter_menu)
-    create(:menu_item, name: "Garlic Bread", price_in_cents: 500, restriction_type: :no_restriction, menu: starter_menu)
-    create(:menu_item, name: "Grilled Chicken", price_in_cents: 1800, restriction_type: :no_restriction, menu: main_menu)
+    create(:menu_item, name: "Bruschetta", price_in_cents: 700, restriction_type: :vegan, menus: [ starter_menu ])
+    create(:menu_item, name: "Garlic Bread", price_in_cents: 500, restriction_type: :no_restriction, menus: [ starter_menu ])
+    create(:menu_item, name: "Grilled Chicken", price_in_cents: 1800, restriction_type: :no_restriction, menus: [ main_menu ])
   end
 
-  describe "GET /menus/:menu_id/menu_items" do
+  describe "GET /restaurants/:restaurant_id/menus/:menu_id/menu_items" do
     it "returns menu items for the given menu" do
-      get "/menus/#{starter_menu.id}/menu_items", as: :json
+      get "/restaurants/#{restaurant.id}/menus/#{starter_menu.id}/menu_items", as: :json
 
       json = JSON.parse(response.body)
 
@@ -22,12 +23,26 @@ RSpec.describe "MenuItemsRequest", type: :request do
     end
 
     it "does not return items from other menus" do
-      get "/menus/#{main_menu.id}/menu_items", as: :json
+      get "/restaurants/#{restaurant.id}/menus/#{main_menu.id}/menu_items", as: :json
 
       json = JSON.parse(response.body)
 
       expect(json.size).to eq(1)
       expect(json.first["name"]).to eq("Grilled Chicken")
+    end
+  end
+
+  describe "GET /restaurants/:restaurant_id/menus/:menu_id/menu_items/:id" do
+    it "returns a specific menu item with price" do
+      item = starter_menu.menu_items.find_by(name: "Bruschetta")
+
+      get "/restaurants/#{restaurant.id}/menus/#{starter_menu.id}/menu_items/#{item.id}", as: :json
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json["name"]).to eq("Bruschetta")
+      expect(json["price"]).to eq(7.0)
     end
   end
 end
