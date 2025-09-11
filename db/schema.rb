f87b97b153903093e76597974ae502244c9f9704
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2025-09-10 21:17:56 -0300

    Add migration to remove menu_id from MenuItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_11_000417) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_001710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,10 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_000417) do
     t.string "name", null: false
     t.integer "price_in_cents", null: false
     t.integer "restriction_type", default: 0, null: false
-    t.bigint "menu_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
     t.index ["name"], name: "index_menu_items_on_name", unique: true
   end
 
@@ -48,6 +46,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_000417) do
     t.index ["name"], name: "index_restaurants_on_name", unique: true
   end
 
-  add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "restaurants"
 end
